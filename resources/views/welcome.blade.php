36861cb4dfcdee17f77dab0789a6b28140d6a38a
Hammad Hussain <hammadasmat@gmail.com>
2022-12-07 23:23:27 +0500

    init commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -206,7 +206,7 @@
         </div>
       </div>
       <div class="row">
-      <div class="col-lg-3 col-xs-6">
+      <div class="col-lg-3 col-xs-6" >
         <!-- small box -->
         <div class="small-box bg-aqua">
           <div class="inner">
